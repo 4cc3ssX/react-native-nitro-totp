e28b602c255927100fc099e0f6da7c62e4f16772
Ryam <heinmyatthu.workspace@gmail.com>
2024-09-27 00:14:50 +0700

    chore: initial project

---
+++ android/src/main/cpp/cpp-adapter.cpp
@@ -0,0 +1,6 @@
+#include <jni.h>
+#include "<<androidCxxLibName>>OnLoad.hpp"
+
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*) {
+  return margelo::nitro::<<androidNamespace>>::initialize(vm);
+}
