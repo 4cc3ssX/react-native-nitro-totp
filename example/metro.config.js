9b555d989ee57fbc4885f84445291e50390ed395
Ryam <heinmyatthu.workspace@gmail.com>
2025-04-20 17:41:20 +0700

    chore: minor changes

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,12 +1,8 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 const path = require('path');
-const pak = require('../package.json');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const pkg = require('../package.json');
 
 const root = path.resolve(__dirname, '..');
-
-const modules = Object.keys({
-  ...pak.peerDependencies,
-});
 
 /**
  * Metro configuration
@@ -15,25 +11,11 @@ const modules = Object.keys({
  * @type {import('@react-native/metro-config').MetroConfig}
  */
 const config = {
-  projectRoot: __dirname,
   watchFolders: [root],
-
-  // We need to make sure that only one version is loaded for peerDependencies
-  // So we block them at the root, and alias them to the versions in example's node_modules
   resolver: {
-    extraNodeModules: modules.reduce((acc, name) => {
-      acc[name] = path.join(__dirname, 'node_modules', name);
-      return acc;
-    }, {}),
-  },
-
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
+    extraNodeModules: {
+      [pkg.name]: root,
+    },
   },
 };
 
